41ced8ba57f58a4d039fb9736c2d9535cf120394
Abraham <abrahamgmacias@hotmail.com>
2021-12-28 21:09:54 -0700

    Set workflow & resources.py classes

--- basic_structure.py
+++ basic_structure.py
@@ -1,43 +1,114 @@
-#!/usr/bin/env python3
 from pycoingecko import CoinGeckoAPI
 from resources import *
-
-cg = CoinGeckoAPI()
-
-# Modify the database class
-class DataBaseMod(DataBase):
-    def __init__(self, db_data, main_table):
-        super().__init__(db_data, main_table)
-
-    def add_token(self, token):
-        if self.main_table_length == None:
-            self.main_table_length = (
-                len(self.get_all_table_data(self.main_table)) + 1
-                )
-        else:
-            self.main_table_length += 1
-
-        self.cursor.execute(
-            f"INSERT INTO {self.main_table} VALUES ('{token.coingecko_id}', '{token.ticker}', '{token.description}', '{token.exchanges}');"
-        )
-        self.connection.commit()
-
-    def get_token(self, coingecko_id):
-        self.cursor.execute(
-            f"SELECT * FROM {self.main_table} WHERE coingecko_id = '{coingecko_id}';"
-        )
-        token_data = self.cursor.fetchall()
-        if len(token_data) == 0:
-            return None
-        else:
-            return token_data[0]    
+import shutil
 
 
 # Initialize database
 db_data = {'db_host': 'localhost', 'db_name': 'crypto', 'db_user': 'postgres', 'db_pass': 'password'}
-database = DataBaseMod(db_data, 'token_data')
+coins_to_analyze = set(["ethereum", "bitcoin", "zelcash"])
+excel_offload_file = 'market_analysis'
+wallet_assets = {"ethereum": 24}
+token_solver = True
+
+# Main and wallet dataframes 
+main_df = pd.DataFrame({'ticker': [], 'description': [], 'exchanges': [], 'current_price': [], 'market_cap': [],
+                        'trading_volume': [], '1h': [], '24h': [], '7d': [], '14d': [], '30d': []})
+wallet_df = pd.DataFrame({'assets': [], 'quantity': [], 'equivalence': [], 'total_usd': [], 'total_mxn': []})
+
+# Solver
+if token_solver == True:
+    # Clean the Excel offload file
+    if '.xlsx' in excel_offload_file:
+        excel_offload_file = (lambda file_name: file_name.split('.'))(excel_offload_file)[0] 
+
+    # Create an Excel file copy
+    shutil.copyfile(f"{excel_offload_file}.xlsx", f"{excel_offload_file} - Copy.xlsx")
+
+    # Initialize Database 
+    db = DataBaseMod(db_data, 'token_data')
+
+    # Create summary and wallet tables
+    main_table = Table('main', main_df, (1, 1))
+    wallet_table = Table('wallet', wallet_df, (1, 13)) 
+
+    # Traverse through tokens of interest...
+    wallet_set = set(wallet_assets.keys())
+    for coin in (wallet_set & coins_to_analyze).union(wallet_set.symmetric_difference(coins_to_analyze)):
+        print(f"Adding {coin}...")
+
+        # Check if coin exists in DB
+        try:
+            coingecko_id, ticker, description, exchanges = db.get_token(coin)
+
+        except Exception:
+            def token_data_gatherer():
+                ticker = input('Ticker: ')
+                description = input('Description: ')
+                exchanges = input('Exchanges: ')
+                print('')
+                return ticker, description, exchanges
+
+            # Gather token data / create token object / add it to the DB
+            ticker, description, exchanges = token_data_gatherer()
+            token = Token(coin, ticker, description, exchanges)
+            db.add_token(token)
 
 
-""" # Trial coin add
-token = Token('bitcoin', 'btc', 'og token', 'literally everywhere')
-database.add_token(token) """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     else:
+    #         # Create existing token object
+    #         token = Token(coingecko_id, ticker, description, exchanges)
+
+    #     # Check if coingecko allows webdriver to navigate
+    #     try: 
+    #         token.set_bot_status()
+
+    #     except Exception:
+    #         print('ABORTED: Website recognizes user as script. Website may be crowded... \n')
+    #         break
+        
+    #     else:
+    #         # Scrape data from coingecko.com
+    #         try: 
+    #             token.scrape_data()
+
+    #         except Exception:
+    #             print(f"Couldn't find {coin}. Verify that it is an official coingecko id... \n")
+
+    #         else:
+    #             # Select token data in dataframe form
+    #             token_df = token.get_data('ticker', 'description', 'exchanges', 'current_price', 'market_cap', 'trading_volume', '1h', '24h', '7d', '14d', '30d', dataframe=True)
+
+    #             # Add token data to the corresponding tables
+    #             main_table.add_line(token_df)
+
+    #             print(f"{coin} added...\n")
+
+    # # Create Excel file access / populate Excel sheet
+    # excel = Excelifier(f'{file}.xlsx', main_table, overwrite_sheets=True)
+    # # excel.add_new_table(wallet_table)
+
+    # # Move assets to sheet / save file
+    # excel.move_to_excel()
+    # excel.save_file()
+
+    # print('Table done. Check Excel...')
--- resources.py
+++ resources.py
@@ -1,12 +1,16 @@
+from urllib.request import Request, urlopen
 from pycoingecko import CoinGeckoAPI
+from openpyxl import load_workbook
+from datetime import date
+import pandas as pd
 import psycopg2
+
 
 class DataBase:
     def __init__(self, db_data, main_table):
         self.connection = self.feed_db_data(db_data)
         self.cursor = self.connection.cursor()
         self.main_table = main_table
-        self.main_table_length = None
 
     def feed_db_data(self, db_data):
         return psycopg2.connect(
@@ -25,10 +29,88 @@ class DataBase:
         self.cursor.close()
         self.connection.close()
 
+
+class DataBaseMod(DataBase):
+    def __init__(self, db_data, main_table):
+        super().__init__(db_data, main_table)
+
+    def add_token(self, token):
+        # Access token data
+        token_data = token.get_token_data()
+
+        # Add token SQL function 
+        self.cursor.execute(
+            f"INSERT INTO {self.main_table} VALUES ('{token_data['id']}', '{token_data['ticker']}', '{token_data['description']}', '{token_data['exchanges']}');"
+        )
+        self.connection.commit()
+
+    def get_token(self, coingecko_id):
+        self.cursor.execute(
+            f"SELECT * FROM {self.main_table} WHERE coingecko_id = '{coingecko_id}';"
+        )
+        token_data = self.cursor.fetchall()
+        if len(token_data) == 0:
+            return None
+        else:
+            return token_data[0]    
+
+
 class Token():
     def __init__(self, coingecko_id, ticker, description, exchanges):
-        self.coingecko_id = coingecko_id
-        self.ticker = ticker
-        self.description = description
-        self.exchanges = exchanges
+        self.token_id = coingecko_id
+        self.token_data = {'id': coingecko_id, 'ticker': ticker, 'description': description, 'exchanges': exchanges}
+
         self.cg = CoinGeckoAPI()
+        self.currency = 'usd'
+
+    def change_currency(self, new_currency):
+        self.currency = new_currency
+
+    def get_price(self, imarket_cap=False, i24hr_vol=False, i24hr_change=False):
+        scraped_token_data = self.cg.get_price(ids=self.token_id, vs_currencies=self.currency, include_market_cap=imarket_cap, include_24hr_vol=i24hr_vol, include_24hr_change=i24hr_change)
+        self.token_data = self.token_data | scraped_token_data[self.token_id]
+
+    def get_token_data(self):
+        return self.token_data
+
+
+class Excelifier():
+    def __init__(self, file_name, main_dataframe, overwrite_sheets=False):
+        # Load or overwrite workbook
+        if overwrite_sheets == False:
+            self.book = load_workbook(file_name)
+            self.writer = pd.ExcelWriter(file_name, engine='openpyxl')
+            self.writer.book = self.book
+            self.writer.sheets = dict((ws.title, ws) for ws in self.book.worksheets)
+        else:
+            self.writer = pd.ExcelWriter(file_name, engine='xlsxwriter')   
+
+        # Create main data frame
+        self.dfs = {'main': main_dataframe}
+
+    def add_new_table(self, table_object):
+        self.dfs[table_object.name] = table_object
+
+    def save_file(self):
+        self.writer.save()
+
+    def move_to_excel(self, table=None, sheet_title=None):
+        if sheet_title == None:
+            title = date.today().strftime("%m-%d")
+
+        for table in self.dfs.values():
+            table.df.to_excel(self.writer, sheet_name=title, startrow=table.coordinates[0], startcol=table.coordinates[1], header=True, index=False)
+            self.worksheet = self.writer.sheets[title]
+
+
+class Table():
+    def __init__(self, name, structure, coordinates):
+        self.name = name
+        self.df = structure
+        self.coordinates = coordinates
+
+    def get_table_data(self): 
+        return self.__dict__
+
+    def add_line(self, line_to_add):
+        self.df = pd.concat([self.df, line_to_add])
